254f7a752ebb06f901ba30f46dffeffc801a19d2
John Bright <jmbright400@gmail.com>
2023-09-28 12:16:42 -0500

    Accomplished Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "John Bright";
+            int yearsOfService = 12;
+            char numberOfBases = '7';
+            bool yearsRemainingOfTotal = false;
+            double yearsRemainingOfEnlistment = 3;
+            decimal yearsRemainingUntilCompletion = 8;
+            
+            Console.WriteLine($"My name is {myName}, I have served in the U.S. Air Force for {yearsOfService} years. Within my {yearsOfService} years of service I have been stationed at {numberOfBases} different military bases. I currently have {yearsRemainingOfEnlistment} years left of my current enlistment and {yearsRemainingUntilCompletion} years remaining until completion of 20 total years.");
+
+
         }
     }
 }
